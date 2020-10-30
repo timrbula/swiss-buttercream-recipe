31bcc8624365f6c02596e408652ec9442ce8edce
Timothy R. Bula <trbula@us.ibm.com>
2020-10-29 22:17:25 -0500

    feat: init commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,188 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Gretchen's Bakery - Swiss Buttercream Recipe</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <h1 className={styles.title}>Gretchen's Swiss Buttercream Recipe</h1>
+        <img className={styles.hero} src="./swiss_buttercream.jpg" />
+        <article>
+          <p>This swiss buttercream recipe is the recipe that put me on the map.</p>
+          <p>
+            Little did I know 5 years ago when I first shared this recipe that I would be ranking #1 in a google search
+            for Swiss Buttercream!
+          </p>
+          <p>
+            Before this most recent change-over to all things Gretchen’s Bakery my recipe was coming up first, even
+            before Food Network Recipes and Martha Stewart!
+          </p>
+          <p>Holy Cannolis!</p>
+          <p>
+            Originally named the World’s Best Swiss Buttercream, because well…..it has made it’s way around the world
+            and back again!
+          </p>
+          <p>
+            So if you are on the fence as to whether or not you should try this recipe…GET DOWN FROM THAT FENCE and TRY
+            IT!
+          </p>
+          <p>
+            PLEASE READ THE Q & A down below before asking your question, because there is a great chance it has already
+            been addressed! THANKS!
+          </p>
+          <h2>Swiss Buttercream Recipe</h2>
+          <div id="easyrecipe-39-0" class="easyrecipe" itemscope="" itemtype="http://schema.org/Recipe">
+            <div itemprop="name" class="ERSName">
+              <u>
+                <strong>Swiss Buttercream Recipe</strong>
+              </u>
+            </div>
+            <div class="ERSClear"></div>
+            <div class="ERSTimes">
+              <div class="ERSTime">
+                <div class="ERSTimeHeading">Prep time</div>
+                <div class="ERSTimeItem">
+                  <time itemprop="prepTime" datetime="PT25M">
+                    25 mins
+                  </time>
+                </div>
+              </div>
+              <div class="ERSTime ERSTimeRight">
+                <div class="ERSTimeHeading">Total time</div>
+                <div class="ERSTimeItem">
+                  <time itemprop="totalTime" datetime="PT25M">
+                    25 mins
+                  </time>
+                </div>
+              </div>
+              <div class="ERSClearLeft"></div>
+            </div>
+            <br />
+            <div itemprop="description" class="ERSSummary">
+              <strong>
+                Please read all the way to the bottom of this blog post before beginning and before asking questions!
+                Thanks!
+              </strong>
+            </div>
+            <br />
+            <div class="divERSHeadItems">
+              <div class="ERSAuthor">Author: Gretchen's Bakery</div>
+              <div class="ERSServes">Serves: 6 Cups: Enough to ice 30 Cupcakes, or Fill and Ice 1- 8" Cake</div>
+            </div>
+            <div class="ERSIngredients">
+              <h2 class="ERSIngredientsHeader ERSHeading">Ingredients</h2>
+              <ul>
+                {" "}
+                <li class="ingredient" itemprop="ingredients">
+                  Fresh Large Egg Whites 180g (6 Large)
+                </li>{" "}
+                <li class="ingredient" itemprop="ingredients">
+                  Granulated Sugar 300g (1 + ½ cups)
+                </li>{" "}
+                <li class="ingredient" itemprop="ingredients">
+                  Confectioners Sugar 240g (2cups) *Domino brand is recommended 10X sugar
+                </li>{" "}
+                <li class="ingredient" itemprop="ingredients">
+                  Solid Vegetable Shortening 170g (3/4 cup)
+                </li>{" "}
+                <li class="ingredient" itemprop="ingredients">
+                  <em>
+                    I prefer to use SWEETEX, it is a{" "}
+                    <a
+                      href="http://web.archive.org/web/20190712094015/http://www.amazon.com/gp/product/B00KCV0G8I/ref=as_li_tl?ie=UTF8&amp;camp=1789&amp;creative=390957&amp;creativeASIN=B00KCV0G8I&amp;linkCode=as2&amp;tag=gretsbake-20&amp;linkId=MBG77TT5BCRX475K"
+                      target="_blank"
+                    >
+                      High Ratio vegetable shortening
+                    </a>
+                  </em>
+                </li>
+                <li class="ingredient" itemprop="ingredients">
+                  Unsalted Butter 454g (2 cups)
+                </li>
+                <li class="ingredient" itemprop="ingredients">
+                  Vanilla Extract Or Vanilla Paste 1 Tablespoon
+                </li>
+                <li class="ingredient" itemprop="ingredients">
+                  Salt - pinch *(I did not mention this on the video, but a dash of salt can heighten all these flavors
+                  and really pull this recipe through- add at the end with the vanilla)
+                </li>
+              </ul>
+              <div class="ERSClear"></div>
+            </div>
+            <div class="ERSInstructions">
+              <h2 class="ERSInstructionsHeader ERSHeading">Instructions</h2>
+              <ol>
+                <li class="instruction" itemprop="recipeInstructions">
+                  Over a double boiler, in a large metal bowl whisk together the granulated sugar and the egg whites
+                  stirring constantly until the mixture reaches approximately 115 degrees F and the sugar grains have
+                  dissolved and are not longer gritty to the touch.
+                </li>
+                <li class="instruction" itemprop="recipeInstructions">
+                  Pour this mixture into the metal bowl of the stand mixer with the WHISK attachment and whip on high
+                  speed until it is thick, glossy and white in color. The peaks should be as firm as firm can be.
+                </li>
+                <li class="instruction" itemprop="recipeInstructions">
+                  In the meantime you can sift your confectioners sugar, and once you have achieved STIFF peaks
+                  meringue, add the confectioners sugar all at once with the mixer on very low speed to avoid having a
+                  snow shower of sugar all over your kitchen!
+                </li>
+                <li class="instruction" itemprop="recipeInstructions">
+                  Once all of the sugar has been incorporated you can turn the mixer speed to med-high until it is all
+                  nicely mixed in.
+                </li>
+                <li class="instruction" itemprop="recipeInstructions">
+                  Be sure your butter and shortening are at room temperature and they are the same consistency. Add it
+                  in about 2-3 additions, but FAST Additions.
+                </li>
+                <li class="instruction" itemprop="recipeInstructions">
+                  Add vanilla extract
+                </li>
+                <li class="instruction" itemprop="recipeInstructions">
+                  Switch to the paddle attachment and blend on low speed until smooth
+                </li>
+              </ol>
+              <div class="ERSClear"></div>
+            </div>
+            <div class="ERSNotesDiv">
+              <h2 class="ERSNotesHeader">Notes</h2>
+              <div class="ERSNotes">
+                <strong>
+                  This Recipe Can Be Stored for 4 days room temperature, 2 weeks refrigerator, 2 months freezer
+                </strong>
+                <br />
+                <br />
+                <u>
+                  <strong>After refrigeration or freezing:</strong>
+                </u>
+                <br />
+                <br />
+                <strong>
+                  Take it out several hours or a day before you need it, and let it get to room temperature.Place entire
+                  recipe back on the mixer and with the PADDLE attachment on low- medium and let it mix.It will look as
+                  if it is broken, and curdled, don't be alarmed this is normal, it will come back together eventually.
+                </strong>
+                <br />
+                <br />
+                <strong>
+                  For those who prefer to use pasteurized egg whites (like Just Whites®) it will take considerably
+                  longer to whip the whites and they will not gain the same volume as fresh whites do.
+                </strong>
+                <br />
+                <br />
+                <strong>
+                  For those using a hand beater you will not get the same volume as the balloon whisk attachment of a
+                  stand mixer, and the whites may take anywhere from 15 minuted to 45 minutes to whip (I have been told)
+                </strong>
+              </div>
+            </div>
+          </div>
+        </article>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
-  )
+  );
 }
